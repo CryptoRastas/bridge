3c173fe86c0b5338beb8c94be967aa9e74c2c5d0
welliton gervickas <wellitogervickas@gmail.com>
2023-12-19 18:16:44 -0300

    fix: task

--- tasks/set-trusted-remote-address.ts
+++ tasks/set-trusted-remote-address.ts
@@ -91,6 +91,7 @@ task('set-trusted-remote-address', 'set trusted remote address')
          */
 
         spinner.start()
+        console.log(`ℹ️ Checking trusted remote is set`)
 
         const trustedRemoteId = await ONFT721Core.trustedRemoteLookup(
           destinationChainConfig.abstractId
@@ -103,6 +104,7 @@ task('set-trusted-remote-address', 'set trusted remote address')
 
         if (!isTrusted) throw new Error('Not trusted')
 
+        spinner.stop()
         console.log(`✅ ONFT721Core contract set trusted remote`)
       } catch (error) {
         spinner.stop()
