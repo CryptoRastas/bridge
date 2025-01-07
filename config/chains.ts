571b5d86fc6b320b74ae87cc858d5ff0574ad100
gervickasjs <wellitogervickas@gmail.com>
2025-01-07 20:02:11 -0300

    feat: update lz endpoint missing

--- config/chains.ts
+++ config/chains.ts
@@ -28,19 +28,6 @@ export const ethereum: Chain = merge(mainnetChain, {
   minGasToTransferAndStoreLocal: 100_000n
 })
 
-export const sepolia: Chain = merge(sepoliaChain, {
-  abstractId: 10161,
-  accounts: evmAccounts,
-  network: 'sepolia',
-  contracts: merge(sepoliaChain.contracts, {
-    lzEndpoint: {
-      address: '0xae92d5aD7583AD66E49A0c67BAd18F6ba52dDDc1' as Address
-    }
-  }),
-  minGasToTransferAndStoreRemote: 260_000n,
-  minGasToTransferAndStoreLocal: 100_000n
-})
-
 export const polygon: Chain = merge(polygonChain, {
   abstractId: 109,
   network: 'matic',
@@ -48,6 +35,33 @@ export const polygon: Chain = merge(polygonChain, {
   contracts: merge(polygonChain.contracts, {
     lzEndpoint: {
       address: '0x3c2269811836af69497E5F486A85D7316753cf62' as Address
+    }
+  }),
+  minGasToTransferAndStoreRemote: 260_000n,
+  minGasToTransferAndStoreLocal: 100_000n
+})
+
+export const base: Chain = merge(baseChain, {
+  abstractId: 184,
+  network: 'base',
+  accounts: evmAccounts,
+  contracts: merge(baseChain.contracts, {
+    lzEndpoint: {
+      address: '0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7' as Address
+    }
+  }),
+  minGasToTransferAndStoreRemote: 260_000n,
+  minGasToTransferAndStoreLocal: 100_000n
+})
+
+/// @dev: testnets
+export const sepolia: Chain = merge(sepoliaChain, {
+  abstractId: 10161,
+  accounts: evmAccounts,
+  network: 'sepolia',
+  contracts: merge(sepoliaChain.contracts, {
+    lzEndpoint: {
+      address: '0xae92d5aD7583AD66E49A0c67BAd18F6ba52dDDc1' as Address
     }
   }),
   minGasToTransferAndStoreRemote: 260_000n,
@@ -63,19 +77,6 @@ export const polygonAmoy: Chain = merge(polygonAmoyChain, {
       address: '0x55370E0fBB5f5b8dAeD978BA1c075a499eB107B8' as Address
     }
   },
-  minGasToTransferAndStoreRemote: 260_000n,
-  minGasToTransferAndStoreLocal: 100_000n
-})
-
-export const base: Chain = merge(baseChain, {
-  abstractId: 184,
-  network: 'base',
-  accounts: evmAccounts,
-  contracts: merge(baseChain.contracts, {
-    lzEndpoint: {
-      address: '0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7' as Address
-    }
-  }),
   minGasToTransferAndStoreRemote: 260_000n,
   minGasToTransferAndStoreLocal: 100_000n
 })
@@ -99,7 +100,7 @@ export const abstractTestnet: Chain = merge(abstractTestnetChain, {
   accounts: evmAccounts,
   contracts: merge(abstractTestnetChain.contracts, {
     lzEndpoint: {
-      address: '0x55370E0fBB5f5b8dAeD978BA1c075a499eB107B8' as Address
+      address: '0x68c1B65211c0d2d39Ed04b2b4F0B6f743A168320' as Address
     }
   }),
   minGasToTransferAndStoreRemote: 260_000n,
